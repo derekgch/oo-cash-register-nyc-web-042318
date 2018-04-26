df38fda4972ddf550222d56aa77675a55f697130
Derek Chen <derek.g.chen@gmail.com>
2018-04-26 01:53:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,10 +10,10 @@ class CashRegister
   end
 
   def add_item(title,price,q=1)
-    q.times
+    q.times do
       self.item_list << title
       self.total += price
-
+    end
   end
 
   def apply_discount
